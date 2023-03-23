c6f5e54536377c7cfbefa2e945d4abed0b7e6c1e
ahamideh <ahamideh18@hotmail.com>
2023-03-22 20:16:38 -0400

    Edit encoder to match phase 2

--- simple_compression/SimpleCompression.py
+++ simple_compression/SimpleCompression.py
@@ -2,79 +2,86 @@ import cv2
 import numpy as np
 import math
 
+
 class Encoder:
     def __init__(self, image):
         self.image = image
-        self.ycbcr = (0,0,0)
+        self.ycbcr = self.rgb2ycbcr(cv2.split(image))
 
     def encode(self):
-        '''
+        """
         Downsamples image in YCbCr colorspace
         Returns: Downsampled Y, Cb, Cr channels as tuple
-        '''
+        """
         B, G, R = cv2.split(self.image)
 
-        Y, Cb, Cr = self.__rgb2ycbcr(R, G, B)
+        Y, Cb, Cr = self.rgb2ycbcr((R, G, B))
 
         # Downsample Y by a factor of 2
-        Y = Y[::2, ::2]
+        Y = self.downsample(Y, 2)
 
         # Downsample Cb and Cr by a factor of 4
-        Cb = Cb[::4, ::4]
-        Cr = Cr[::4, ::4]
+        Cb = self.downsample(Cb, 4)
+        Cr = self.downsample(Cr, 4)
 
         self.ycbcr = (Y, Cb, Cr)
-    
-    def __rgb2ycbcr(self, R, G, B):
-        '''
+
+    def rgb2ycbcr(self, rgb):
+        """
         Convert RGB colorspace to YCbCr colorspace
         Parameters: R, G, B channels
         Returns: Y, Cb, Cr channels as tuple
-        '''
-        Y = 0.257*R + 0.504*G + 0.098*B + 16
-        Cb = -0.148*R - 0.291*G + 0.439*B + 128
-        Cr = 0.439*R - 0.368*G - 0.071*B + 128
+        """
+        R, G, B = rgb
+        Y = 0.257 * R + 0.504 * G + 0.098 * B + 16
+        Cb = -0.148 * R - 0.291 * G + 0.439 * B + 128
+        Cr = 0.439 * R - 0.368 * G - 0.071 * B + 128
 
         Y = np.clip(Y, 0, 255).astype(np.uint8)
         Cb = np.clip(Cb, 0, 255).astype(np.uint8)
         Cr = np.clip(Cr, 0, 255).astype(np.uint8)
 
         return (Y, Cb, Cr)
-    
+
+    def downsample(self, channel, factor):
+        return channel[::factor, ::factor]
+
     def get_ycbcr(self):
         return self.ycbcr
-    
+
+
 class Decoder:
     def __init__(self, ycbcr):
         self.ycbcr = ycbcr
 
     def decode(self, size):
-        '''
+        """
         Upsamples image in YCbCr colorspace
         Parameters: Size of image to upsample to
         Returns: Reconstructed image
-        '''
+        """
         # Upsample Y by a factor of 2
-        Y = self.__bilinear(self.ycbcr[0], size)
+        Y = self.bilinear(self.ycbcr[0], size)
 
         # Upsample Cb and Cr by a factor of 4
-        Cb = self.__bilinear(self.ycbcr[1], size)
-        Cr = self.__bilinear(self.ycbcr[2], size)
+        Cb = self.bilinear(self.ycbcr[1], size)
+        Cr = self.bilinear(self.ycbcr[2], size)
 
-        R, G, B = self.__ycbcr2rgb(Y, Cb, Cr)
+        R, G, B = self.ycbcr2rgb((Y, Cb, Cr))
 
         self.image = cv2.merge((B, G, R))
         return cv2.merge((B, G, R))
 
-    def __ycbcr2rgb(self, Y, Cb, Cr):
-        '''
+    def ycbcr2rgb(self, ycbcr):
+        """
         Convert YCbCr colorspace to RGB colorspace
         Parameters: Y, Cb, Cr channels
         Returns: R, G, B channels as tuple
-        '''
-        R = 1.164*(Y - 16) + 1.596*(Cr - 128)
-        G = 1.164*(Y - 16) - 0.392*(Cb - 128) - 0.813*(Cr - 128)
-        B = 1.164*(Y - 16) + 2.017*(Cb - 128)
+        """
+        Y, Cb, Cr = ycbcr
+        R = 1.164 * (Y - 16) + 1.596 * (Cr - 128)
+        G = 1.164 * (Y - 16) - 0.392 * (Cb - 128) - 0.813 * (Cr - 128)
+        B = 1.164 * (Y - 16) + 2.017 * (Cb - 128)
 
         R = np.clip(R, 0, 255).astype(np.uint8)
         G = np.clip(G, 0, 255).astype(np.uint8)
@@ -82,12 +89,12 @@ class Decoder:
 
         return (R, G, B)
 
-    def __bilinear(self, image, dimension):
-        '''
+    def bilinear(self, image, dimension):
+        """
         Bilinear interpolation
         Parameters: Downsampled channel and dimension to upsample to
         Returns: Upsampled channel
-        '''
+        """
         height, width = image.shape[:2]
         new_height, new_width = dimension
 
@@ -118,10 +125,12 @@ class Decoder:
                 x_weight = (x_ratio * j) - x1
                 y_weight = (y_ratio * i) - y1
 
-                pixel = p11 * (1 - x_weight) * (1 - y_weight) + \
-                p12 * x_weight * (1 - y_weight) + \
-                p21 * y_weight * (1 - x_weight) + \
-                p22 * x_weight * y_weight
+                pixel = (
+                    p11 * (1 - x_weight) * (1 - y_weight)
+                    + p12 * x_weight * (1 - y_weight)
+                    + p21 * y_weight * (1 - x_weight)
+                    + p22 * x_weight * y_weight
+                )
 
                 upsampled[i, j] = pixel.astype(np.uint8)
         return upsampled
@@ -133,10 +142,11 @@ class Decoder:
     def get_image(self):
         return self.image
 
+
 def main():
-    PATH = 'motorcycles.png'
-    FILENAME = PATH.split('/')[-1].split('.')[0]
-    EXTENSION = PATH.split('.')[-1]
+    PATH = "motorcycles.png"
+    FILENAME = PATH.split("/")[-1].split(".")[0]
+    EXTENSION = PATH.split(".")[-1]
     original_image = cv2.imread(PATH, 1)
 
     encoder = Encoder(original_image)
@@ -148,15 +158,16 @@ def main():
     final_image = decoder.get_image()
 
     psnr = decoder.get_psnr(original_image)
-    print('PSNR:', psnr)
+    print("PSNR:", psnr)
     cv2.imwrite(f"{FILENAME}_upsampled.{EXTENSION}", final_image)
-    cv2.imshow('Y', downsampled_image[0])
-    cv2.imshow('Cb', downsampled_image[1])
-    cv2.imshow('Cr', downsampled_image[2])
-    cv2.imshow('Original', original_image)
-    cv2.imshow('Upsampled', final_image)
+    cv2.imshow("Y", downsampled_image[0])
+    cv2.imshow("Cb", downsampled_image[1])
+    cv2.imshow("Cr", downsampled_image[2])
+    cv2.imshow("Original", original_image)
+    cv2.imshow("Upsampled", final_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
